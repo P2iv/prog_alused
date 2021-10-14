4e433e81fa050025f8880a0dce0c3c93733d68bb
Päiv <paiv.kleimann@khk.ee>
2021-10-14 09:42:52 +0300

    see on teksti väljastamine ja kommenteerimine muutujate deklareerimine, omistamine ja kasutamine

--- app.js
+++ app.js
@@ -12,3 +12,18 @@ console.log("Tere Maailm")
 console.log('See on teine rida')
 
 console.log('See ' + 'on ' + 'liitlause')
+console.log('Aastal 2016 valiti Eesti presidendiks Kersti Kaljulaid')
+
+//muutujad ja andmetüübid
+//1. deklareerimine
+// let muutujanimi
+//2. väärtuse omistamine
+//muutujanimi = väärtus
+
+
+let aasta = 2016
+
+
+let president = 'Kersti Kaljulaid'
+
+console.log(aasta + '. Eesti presidendiks valiti ' + president)
