86ad59bcd78be483eb139116b1ab4051a2eaaa79
Päiv <paiv.kleimann@khk.ee>
2021-10-25 16:12:29 +0300

    astendamine

---
+++ astendamine.js
@@ -0,0 +1,9 @@
+
+var base = prompt("Sisesta astme alus");
+
+if (base != null) {
+    exponent = window.prompt( "Sisesta aste");
+    const result = base ** exponent;
+    console.log(result);
+
+}
